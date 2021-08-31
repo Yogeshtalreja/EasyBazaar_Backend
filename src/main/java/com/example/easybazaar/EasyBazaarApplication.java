8ebfcebbd861cb6ad622e46a4f3a8737d8b85b05
bitjira <yogesh.talreja@i2p.com.pk>
2021-08-31 13:14:41 +0500

    Intial Commit

---
+++ src/main/java/com/example/easybazaar/EasyBazaarApplication.java
@@ -0,0 +1,13 @@
+package com.example.easybazaar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyBazaarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyBazaarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/easybazaar/EasyBazaarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.easybazaar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyBazaarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
